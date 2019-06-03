24728ba84df75dd31360123a326b697651d84fd4
Minh Duc <minhduc.tran.sgu@gmail.com>
2019-06-04 00:11:19 +0700

    First commit

---
+++ src/components/Todo.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-flex xs12 mb-2>
+    <v-card light color="white">
+      <div class="view">
+        <v-layout row wrap>
+          <v-flex xs8 pa-2>
+            <span @dblclick="onEdit(todo)">{{todo.title}}</span>
+          </v-flex>
+          <v-flex xs4 text-xs-right>
+            <v-btn color="error" @click="onRemove(todo)">Delete</v-btn>
+          </v-flex>
+        </v-layout>
+      </div>
+      <div class="edit px-2">
+        <v-text-field
+          name="name"
+          single-line
+          autocomplete="off"
+          v-model="todo.title"
+          @keyup.esc="onCancelEdit(todo)"
+          @keyup.enter="onDoneEdit(todo)"
+          @blur="onDoneEdit(todo)"
+        ></v-text-field>
+      </div>
+    </v-card>
+  </v-flex>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class Todo extends Vue {
+  @Prop() todo!: any;
+  @Prop() onRemove!: any;
+  @Prop() onEdit!: any;
+  @Prop() onCancelEdit!: any;
+  @Prop() onDoneEdit!: any;
+}
+</script>
+
+<style lang="css" scoped>
+.view {
+  display: block;
+}
+.edit {
+  display: none;
+}
+.editing .view {
+  display: none;
+}
+.editing .edit {
+  display: block;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,86 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="home" id="home">
+    <v-container grid-list-xs>
+      <h1 class="primary--text display-4">Todo list</h1>
+      <v-text-field
+        name="name"
+        label="What needs to be done?"
+        single-line
+        autocomplete="off"
+        v-model="newTodo"
+        @keyup.enter="addTodo"
+      ></v-text-field>
+      <v-layout row wrap>
+        <Todo
+          v-for="(todo, index) in todos"
+          :key="index"
+          :class="{editing: todo == editedTodo}"
+          :todo="todo"
+          :onRemove="removeTodo"
+          :onEdit="editTodo"
+          :onCancelEdit="cancelEdit"
+          :onDoneEdit="doneEdit"
+        />
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import Todo from "@/components/Todo.vue";
 
 @Component({
   components: {
-    HelloWorld,
-  },
+    Todo
+  }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  todos: Array<any> = [
+    { id: 0, title: "Item 1", completed: false },
+    { id: 0, title: "Item 2", completed: false },
+    { id: 0, title: "Item 3", completed: false },
+    { id: 0, title: "Item 4", completed: false }
+  ];
+  newTodo: any = "";
+  editedTodo: any = null;
+  beforeEditCache: any = null;
+
+  addTodo() {
+    const value = this.newTodo && this.newTodo.trim();
+    if (!value) {
+      return;
+    }
+
+    this.todos.push({
+      id: new Date().getTime(),
+      title: value,
+      complete: false
+    });
+    this.newTodo = "";
+  }
+
+  removeTodo(todo: any) {
+    const index = this.todos.indexOf(todo);
+    this.todos.splice(index, 1);
+  }
+
+  editTodo(todo: any) {
+    this.beforeEditCache = todo.title;
+    this.editedTodo = todo;
+  }
+
+  doneEdit(todo: any) {
+    this.editedTodo = null;
+    todo.title = todo.title.trim();
+    if (!todo.title) {
+      this.removeTodo(todo);
+    }
+  }
+
+  cancelEdit(todo: any) {
+    this.editedTodo = null;
+    todo.title = this.beforeEditCache;
+  }
+}
 </script>
